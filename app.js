40783e2f7bd18b26957a82ea79a6a7c2decbe85f
dawidkrem <dawidkrem@gmail.com>
2020-10-08 00:14:40 +0200

    First issue

---
+++ app.js
@@ -0,0 +1,72 @@
+var createError = require('http-errors');
+var cookieSession = require('cookie-session');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var config = require('./config');
+const mongoose = require('mongoose');
+
+mongoose.connect(config.db, {
+  useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+var indexRouter = require('./routes/index');
+var newsRouter = require('./routes/news');
+var quizRouter = require('./routes/quiz');
+var adminRouter = require('./routes/admin');
+
+//mongodb+srv://admin:bNLHRe85HnqX23rt@cluster0.bxtdl.mongodb.net/test?retryWrites=true&w=majority
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+  name: 'session',
+  keys: config.keySession,
+  maxAge: config.maxAgeSession
+}))
+
+
+app.use(function (req, res, next) {
+  res.locals.path = req.path;
+
+  next();
+});
+
+app.use('/', indexRouter);
+app.use('/news', newsRouter);
+app.use('/quiz', quizRouter);
+app.use('/admin', adminRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
