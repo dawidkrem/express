21b9280530174e2d754fbd76dc2cf99f54b1ac50
dawidkrem <dawidkrem@gmail.com>
2020-10-08 12:37:52 +0200

    Sortowanie i widok News skonczone

--- routes/news.js
+++ routes/news.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const News = require('../models/news');
 
 /* GET home page. */
 router.get('/', (req, res) => {
-    res.render('news', {
-        title: 'News'
+    const search = req.query.search || '';
+
+    const findNews = News
+        .find({
+            title: new RegExp(search.trim(), 'i')
+        })
+        .sort({
+            created: -1
+        });
+
+    findNews.exec((err, data) => {
+        res.render('news', {
+            title: 'News',
+            data,
+            search
+        });
     });
+
+
 });
 
 module.exports = router;
