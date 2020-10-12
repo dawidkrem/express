ba7e20d367714836966ee5a7ab63517f5264dc25
dawidkrem <dawidkrem@gmail.com>
2020-10-12 21:08:37 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    db: 'mongodb+srv://admin:bNLHRe85HnqX23rt@cluster0.bxtdl.mongodb.net/test?retryWrites=true&w=majority',
-    keySession: ['TWOJKLUCZ'],
-    maxAgeSession: 24 * 60 * 60 * 1000
-}
